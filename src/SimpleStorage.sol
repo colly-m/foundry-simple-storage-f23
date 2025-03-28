1a54014a4cbb633e9f699b6dfe3701209b93eaa8
colly-m <sindanicolly@gmail.com>
2025-03-28 13:33:43 +0300

    Foundry dos update

--- src/SimpleStorage.sol
+++ src/SimpleStorage.sol
@@ -1,60 +1,38 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
 
-import {PriceConverter} from "./PriceConverter.sol";
+pragma solidity ^0.8.19;
 
-error NotOwner();
+// pragma solidity ^0.8.0;
+// pragma solidity >=0.8.0 <0.9.0;
 
-contract FundMe {
-    using PriceConverter for uint256;
+contract SimpleStorage {
+    uint256 myFavoriteNumber;
 
-    uint256 public constant MINIMUM_USD = 5 * 1e18; // 5e18
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+    // uint256[] public anArray;
+    Person[] public listOfPeople;
 
-    address[] public funders;
-    mapping (address funder => uint256 amountFunded) public addressToAmountFunded;
+    mapping(string => uint256) public nameToFavoriteNumber;
 
-    address public immutable i_owner;
-
-    constructor() {
-        i_owner = msg.sender;
+    function store(uint256 _favoriteNumber) public virtual {
+        myFavoriteNumber = _favoriteNumber;
     }
 
-    function fund() public payable {
-        // msg.value.getConversionRate();
-        require(msg.value.getConversionRate() >= MINIMUM_USD, "didn't send enough ETH");
-        funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] += msg.value;
+    function retrieve() public view returns (uint256) {
+        return myFavoriteNumber;
     }
 
-    function withdraw() public {
-        // require(msg.sender == owner, "Cannot withdraw from other addresses.");
-        //for(/* starting indes, ending index, step amount */)
-        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
-            address funder = funders[funderIndex];
-            addressToAmountFunded[funder] = 0;
-        }
-        funders = new address[](0);
-        // transfer
-        // payable(msg.sender).transfer(address(this).balance);
-        // send
-        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
-        // require(sendSuccess, "Send failed");
-        // call
-        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
-        require(callSuccess, "Call failed");
-    }
-
-    modifier onlyOwner() {
-        // require(msg.sender == i_owner, "Sender is not owner");
-        if(msg.sender != i_owner) { revert NotOwner(); }
-        _;
-    }
-
-    receive() external payable {
-        fund();
-    }
-
-    fallback() external payable {
-        fund();
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push(Person(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
     }
 }
+
+contract SimpleStorage2 {}
+
+contract SimpleStorage3 {}
+
+contract SimpleStorage4 {}
